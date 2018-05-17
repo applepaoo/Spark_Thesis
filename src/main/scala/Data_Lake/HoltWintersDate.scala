f81e91b704a1e986dac5fc511a37a9afad74d7ab
applepaoo <applepaoo@gmail.com>
2018-05-17 17:54:36 +0800

    no message

--- src/main/scala/Data_Lake/HoltWintersDate.scala
+++ src/main/scala/Data_Lake/HoltWintersDate.scala
@@ -1,11 +1,12 @@
 package Data_Lake
 
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import java.util.{Calendar, Date, Properties}
 
 import com.cloudera.sparkts.models.HoltWinters
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -48,16 +49,46 @@ object HoltWintersDate {
     def trainAndPredict(a: String) = {
       //取得MySQL資料並利用HoltWinters預測未來兩天
       var test = spark.sql(a)
+      println("querty:" + a)
       println("訓練集")
       test.show(48)
       val dataTrain = test.select("x").rdd.map(r => r(0)).map(_.toString).map(_.toDouble).collect()
       val ts = Vectors.dense(dataTrain)
       val hModel = HoltWinters.fitModel(ts, 24, "additive", "BOBYQA")
       val forecast = hModel.forecast(ts, ts)
+      val forecastArray = forecast.toArray
       println("開始預測")
-      forecast.toArray.foreach(println)
-      println("共預測了" + forecast.toArray.length + "筆資料")
+      forecastArray.foreach(println)
+      println("共預測了" + forecastArray.length + "筆資料")
+
+      val writeToMySQLArray = Array.ofDim[String](24, 4) //宣告存放預測資料的二維陣列
+
+      for (i <- 0 to 23) {
+        //填入預測資料至二維陣列
+
+        writeToMySQLArray(i)(0) = getNowDate() //時間
+        writeToMySQLArray(i)(1) = i.toString //小時
+        writeToMySQLArray(i)(2) = "LIB-4" //地點
+        writeToMySQLArray(i)(3) = forecastArray(i).toString //預測電力度數P值
+
+      }
+
+      //寫入至MySQL
+      println("寫入開始...")
+      val predictRDD = spark.sparkContext.parallelize(writeToMySQLArray)//創建RDD
+      val schema = StructType(List(StructField("date", StringType, true), StructField("hr", IntegerType, true), StructField("Meter_id", StringType, true), StructField("P", DoubleType, true)))//定義schema
+      val rowRDD = predictRDD.map(p => Row(p(0).toString, p(1).toInt, p(2).toString, p(3).toDouble))//RDD指定元素型態
+      val predictDF = spark.createDataFrame(rowRDD, schema)//
+      //設置寫入MySQL相關變量
+      val prop = new Properties()
+      prop.put("user", "hpc")
+      prop.put("password", "hpcverygood")
+      prop.put("driver", "com.mysql.jdbc.Driver")
+      predictDF.write.mode("append").jdbc("jdbc:mysql://120.109.150.175:3306/power", "power.PowerHourPredict", prop)//寫入
+      println("已寫入至MySQL")
+
     }
+
 
     val sdf = new SimpleDateFormat("yyyy-MM-dd")
     //規定好日期格式
